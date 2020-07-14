374f5568e80e6b589cf0793af236896bc5f18cb3
porkostomus <porkostomus@mail.com>
2020-07-14 14:16:28 -0700

    clean up unused code

--- src/mecca_math/app.cljs
+++ src/mecca_math/app.cljs
@@ -1,82 +1,67 @@
 (ns mecca-math.app
   (:require [reagent.core :as r]
-            [mecca-math.letters :as letters]
-            [mecca-math.numbers :as numbers]
-            [mecca-math.symbols :as symbols]
-            ["katex" :as katex]))
+            ["katex" :as katex]
+            [clojure.edn :as edn]))
 
-(defonce latex (r/atom "\\huge{\\pink{x}=\\dfrac{-\\blue{b}\\pm\\sqrt{\\blue{b}\\purple{^2}-4\\red{a}\\orange{c}}}{2\\red{a}}}"))
+(defonce variable (r/atom "x"))
+(defonce coeffs (r/atom "[2 7 -6]"))
+(defonce latex (r/atom "2x^2+7x-6"))
 
 (defn latex->html [latex]
   (.renderToString katex latex (clj->js {:throwOnError false})))
 
-(defn svg-paths
-  ([paths]
-   (svg-paths nil paths 0 0 1))
-  ([attrs paths]
-   (svg-paths attrs paths 0 0 1))
-  ([paths x y]
-   (svg-paths nil paths x y 1))
-  ([paths x y scale]
-   (svg-paths nil paths x y scale))
-  ([attrs paths x y scale]
-   (into [:g (merge attrs
-                    {:transform (str "scale(" scale ") translate(" x "," y ")")})]
-         (for [[color path] paths]
-           ^{:key [color path]}
-           [:path {:stroke color
-                   :d      path}]))))
+(defn poly->latex
+  "Takes a sequence of polynomial terms in dense-form
+   and a variable letter, returns a string of LaTeX output."
+  [coeffs v]
+  (let [s (apply str
+                 (loop [terms coeffs tex []]
+                   (if (empty? terms)
+                     tex
+                     (recur (rest terms)
+                            (conj tex
+                                  (str
+                                   (if (pos? (first terms))
+                                     "+"
+                                     "-")
+                                   (Math/abs (first terms))
+                                   (when (> (count terms) 1)
+                                     v)
+                                   (when (> (count terms) 2)
+                                     (str "^" (dec (count terms))))))))))]
+    (if (= "+" (first s))
+      (subs s 1)
+      s)))
 
-(def selected-cell (atom nil))
-
-(def mouse-over-cell (atom nil))
-
-(defn letter-row-1 []
-  (into [:g]
-        (for [x (range 10)]
-          ^{:key [x]}
-          [:path {:d ((get [:q :w :e :r :t :y :u :i :o :p] x) letters/paths) :stroke "black"
-                  :transform (str "matrix(1 0 0 -1 0 0) scale(0.05) translate(" (+ 170 (* 1060 x)) "," 2150 ")")}])))
-
-(defn letter-row-2 []
-  (into [:g]
-        (for [x (range 9)]
-          ^{:key [x]}
-          [:path {:d ((get [:a :s :d :f :g :h :j :k :l] x) letters/paths) :stroke "black"
-                  :transform (str "matrix(1 0 0 -1 0 0) scale(0.05) translate(" (+ 170 (* 1060 x)) "," 1060 ")")}])))
-
-(defn letter-row-3 []
-  (into [:g]
-        (for [x (range 7)]
-          ^{:key [x]}
-          [:path {:d ((get [:z :x :c :v :b :n :m] x) letters/paths) :stroke "black"
-                  :transform (str "matrix(1 0 0 -1 0 0) scale(0.05) translate(" (+ 150 (* 1060 x)) "," -20 ")")}])))
-
-(defn letter-cells []
-  (into [:g]
-        (for [x (range 10)
-              y (range 3)]
-          ^{:key [x y]}
-          [:path {:d (:equals symbols/paths) :stroke "black"
-                  :transform (str "matrix(1 0 0 -1 0 0) scale(0.05) translate(" (+ 100 (* 1060 x)) "," (* 1060 y) ")")}])))
-
-(defn cells []
-  (into [:g]
-        (for [x (range 10)
-              y (range 3)]
-          ^{:key [x y]}
-          [:rect {:width 50.5 :height 50.5 :x (* 53.1 x) :y (+ -145 (* 53.1 y)) 
-                  :stroke-width 1 :stroke "black" :fill "white" :rx "10"}])))
+(comment
+  (poly->latex [2 7 -6] "x")
+  )
 
 (defn app []
   [:div#app
-   [:h1 "MECCA-Math"]
+   [:h2 "Coefficients:"]
+   [:div [:textarea
+          {:on-change #(do (reset! coeffs (-> % .-target .-value))
+                           (reset! latex (poly->latex (edn/read-string (-> % .-target .-value)) @variable)))
+           :value @coeffs
+           :style {:resize "none"
+                   :height "20px"
+                   :width "34%"}}]]
+   [:h2 "Variable:"]
+   [:textarea
+    {:on-change #(reset! variable (-> % .-target .-value))
+     :value @variable
+     :style {:resize "none"
+             :height "20px"
+             :width "5%"}}]
+   [:h2 "LaTeX:"]
    [:div [:textarea
           {:on-change #(reset! latex (-> % .-target .-value))
            :value @latex
            :style {:resize "none"
                    :height "100px"
                    :width "75%"}}]]
+   [:h2 "Rendered output:"]
    [:div {:dangerouslySetInnerHTML {:__html (latex->html @latex)}}]])
 
 (defn render []
